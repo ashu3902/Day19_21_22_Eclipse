482840407062d17c03c0e64550a22ee0dc1f8513
ashu3902 <moreashutosh1997@gmail.com>
2020-10-28 18:01:00 +0530

    Added: Welcome message

---
+++ EmployeeWageComputation/src/EmployeeWageComputation/EmployeeWageCalculation.java
@@ -0,0 +1,9 @@
+package EmployeeWageComputation;
+public class EmployeeWageCalculation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is employee wage computation problem");
+	}
+
+}
